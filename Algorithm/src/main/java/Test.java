b9b66f57004f91e2b3d7201171269d725f955855
yve <631555801@qq.com>
2022-11-26 02:49:51 +1300

    update

--- Algorithm/src/main/java/Test.java
+++ Algorithm/src/main/java/Test.java
@@ -1,8 +1,10 @@
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Test {
     public static void main(String[] args) {
-
+        ConcurrentHashMap;
+        HashMap
     }
 }
